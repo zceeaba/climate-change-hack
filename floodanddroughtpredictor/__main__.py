0e848379a3ffaf12a9034fd5dbbed55d16fdf8f3
zceeaba <abhishekh.baskaran@thoughtriver.com>
2021-03-17 12:16:50 +0000

    Made furthere refactoring to univariate lstm

--- floodanddroughtpredictor/__main__.py
+++ floodanddroughtpredictor/__main__.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
 from floodanddroughtpredictor.data_import import load_data
-from floodanddroughtpredictor.data_process import run_lstm
+from floodanddroughtpredictor.data_analysis import run_lstm
 
 if __name__ == '__main__':
     rain_data = load_data('pr_1991_2016_NER.csv')
     temp_data = load_data('tas_1991_2016_NER.csv')
-    run_lstm(temp_data)
+    run_lstm(temp_data, 'temperature')
+    run_lstm(rain_data, 'rain')
